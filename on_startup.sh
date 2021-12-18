10ffc9e6cc39a8d15af1a080d919b945b8d1d065
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-12-17 21:04:20 -0500

    improve startup script robustness

--- on_startup.sh
+++ on_startup.sh
@@ -8,4 +8,15 @@
 
 # This script assumes the project is located in the "~/streamer" folder!
 
-tmux new-session -d -s streamer 'cd ~/streamer && ./run.sh'
+# Make sure the start script is executable
+chmod +x run.sh
+
+# A simple startup instruction
+# tmux new-session -d -s streamer 'cd ~/streamer && ./run.sh'
+
+# A more robust aproach that supports virtual environments
+tmux new-session -s cam -d
+tmux send-keys -t "cam:0" "/bin/bash" Enter
+tmux send-keys -t "cam:0" "cd /home/pi/streamer" Enter
+tmux send-keys -t "cam:0" "source venv/bin/activate" Enter
+tmux send-keys -t "cam:0" "sudo ./run.sh" Enter
